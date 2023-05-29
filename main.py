089c772a7700853f1816874dc40ad657f9121268
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-29 23:10:31 +0300

    #2

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-#1
-class InvalidUrlError(Exception):
-    def __init__(self, url):
-        self.url = url
-        super().__init__()
-
-def fetch_data_from_url(url):
-    try:
-        if not is_valid_url(url):
-            raise InvalidUrlError(url)
-        return data
-    except Exception as e:
-        raise InvalidUrlError(url) from e
-def is_valid_url(url):
-    return True
-url = "https://example.com"
+class UserNotFoundError(Exception):
+    pass
+class User:
+    def __init__(self, username):
+        self.username = username
+class UserDatabase:
+    def __init__(self):
+        self.users = {}
+    def add_user(self, user):
+        self.users[user.username] = user
+    def get_user(self, username):
+        if username in self.users:
+            return self.users[username]
+        else:
+            raise UserNotFoundError(f"User '{username}' not found")
+db = UserDatabase()
+user1 = User("максік")
+user2 = User("діма")
+db.add_user(user1)
+db.add_user(user2)
 try:
-    fetch_data_from_url(url)
-    print("Дані отримано успішно.")
-except InvalidUrlError as e:
-    print(f"Помилка при запиті URL: {e.url}")
-''
+    user = db.get_user("максік")
+    print(f"Username: {user.username}")
+except UserNotFoundError as e:
+    print(str(e))
+
+try:
+    user = db.get_user("глеп")
+    print(f"Username: {user.username}")
+except UserNotFoundError as e:
+    print(str(e))
